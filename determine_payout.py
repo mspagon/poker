9aa6102088f5be299e2faae93a4c5a87fd8dc08d
bragon <bspagon@gmail.com>
2022-04-10 14:36:27 -0500

    Add payout logic and tests

--- determine_payout.py
+++ determine_payout.py
@@ -1,31 +1,21 @@
-from typing import List
-from collections import namedtuple
-
 import random
+from collections import Counter, namedtuple
+from typing import List, Tuple
 
-# initial_bet = [1, 2, 3, 4, 5]
-# payout = {
-#     "Royal Flush": [250, 500, 750, 1000, 4000],
-#     "Straight Flush": [50, 100, 150, 200, 250],
-#     "Four of a Kind": [25, 50, 75, 100, 125],
-#     "Full House": [9, 18, 27, 36, 45],
-#     "Flush": [6, 12, 18, 24, 30],
-#     "Straight": [4, 8, 12, 16, 20],
-#     "Three of a Kind": [3, 6, 9, 12, 15],
-#     "Two Pair": [2, 4, 6, 8, 10],
-#     "Jacks or Better": [1, 2, 3, 4, 5],
-# }
+import constants
 
-Card = namedtuple('Card', 'suit value')
+Card = namedtuple("Card", ["suit", "value"])
 
-
-# Deck class currently doesn't put cards back in the deck, so you need to re-instantiate it every time you deal a hand.
+# Deck class currently doesn't put cards back in the deck, so you need to
+# re-instantiate it every time you deal a hand.
 class Deck:
-    suits = ['hearts', 'diamonds', 'clubs', 'spades']
-    values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    suits = ["hearts", "diamonds", "clubs", "spades"]
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
     def __init__(self):
-        self.cards = [Card(suit, value) for suit in Deck.suits for value in Deck.values]
+        self.cards = [
+            Card(suit, value) for suit in Deck.suits for value in Deck.values
+        ]
         self.shuffle()
 
     def shuffle(self):
@@ -35,27 +25,79 @@ class Deck:
         return self.cards.pop()
 
 
-def determine_payout(hand: List['Card'], initial_bet: int) -> (str, int):
-    print(hand)
-    return "Three of a Kind", 25
+def build_value_arr(hand: List[Card]) -> List[bool]:
+    """Return a index of booleans corresponding to card presence."""
+    value_arr = [None] * 14
+
+    for _, value in hand:
+        indices = constants.VALUE_TO_ARR_MAP[value]
+        for index in indices:
+            value_arr[index] = True
+
+    return value_arr
+
+
+def determine_payout(hand: List[Card], initial_bet: int) -> Tuple[str, int]:
+    """Return the hand result and payout amount."""
+    value_arr = build_value_arr(hand)
+    is_straight = any(
+        [all(value_arr[i : i + 5]) for i in range(len(value_arr) - 4)]
+    )
+    num_suits_in_hand = len({card[constants.SUIT] for card in hand})
+    num_values_in_hand = len({card[constants.VALUE] for card in hand})
+    most_of_a_kind = max(
+        Counter([card[constants.VALUE] for card in hand]).values()
+    )
+
+    hand_result = constants.LOSE
+
+    if num_suits_in_hand == 1 and all(value_arr[-5:]):
+        hand_result = constants.ROYAL_FLUSH
+
+    elif num_suits_in_hand == 1 and is_straight:
+        hand_result = constants.STRAIGHT_FLUSH
+
+    elif num_values_in_hand == 2:
+        if most_of_a_kind == 4:
+            hand_result = constants.FOUR_OF_A_KIND
+        else:
+            hand_result = constants.FULL_HOUSE
+
+    elif num_suits_in_hand == 1:
+        hand_result = constants.FLUSH
+
+    elif is_straight:
+        hand_result = constants.STRAIGHT
+
+    elif most_of_a_kind == 3:
+        hand_result = constants.THREE_OF_A_KIND
+
+    elif num_values_in_hand == 3 and most_of_a_kind == 2:
+        hand_result = constants.TWO_PAIR
+
+    elif any(value_arr[constants.JACK_INDEX :]):
+        hand_result = constants.JACKS_OR_BETTER
+
+    return (
+        hand_result,
+        constants.PAYOUTS[hand_result][constants.BET_AMOUNTS[initial_bet]],
+    )
 
 
 def main():
     deck = Deck()
     hand = [deck.deal_one() for _ in range(5)]
-    determine_payout(hand, 1)
 
     test_hand = [
-        Card('diamond', '2'),
-        Card('diamond', '3'),
-        Card('diamond', '4'),
-        Card('diamond', '5'),
-        Card('diamond', '6')
+        Card("hearts", "A"),
+        Card("hearts", "3"),
+        Card("diamonds", "3"),
+        Card("diamonds", "5"),
+        Card("diamonds", "4"),
     ]
 
-    # You can write simple asserts to test your code
-    assert (determine_payout(test_hand, 5) == ('Straight Flush', 250))
+    determine_payout(test_hand, 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
